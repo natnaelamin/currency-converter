23c8b4e0dfb06d3e52b54b7fabd33f86f7d3c619
Natnael Amin <natnaelamin24@gmail.com>
2023-04-05 02:17:21 +0300

    firstt

---
+++ main.js
@@ -0,0 +1,54 @@
+let api = `https://v6.exchangerate-api.com/v6/9a750063c69dd763342bc8af/latest/USD`;
+const fromDropDown = document.getElementById("from-select");
+const toDropDown = document.getElementById("to-select");
+
+//Create dropdown from the currencies array
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  fromDropDown.add(option);
+});
+
+//Repeat same thing for the other dropdown
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  toDropDown.add(option);
+});
+
+//Set default values
+fromDropDown.value = "USD";
+toDropDown.value = "ETB";
+
+let convertCurrency = () => {
+  
+  const amount = document.querySelector("#amount").value;
+  const fromCurrency = fromDropDown.value;
+  const toCurrency = toDropDown.value;
+
+
+  function fetchapi(){
+    fetch(api)
+      .then((resp) => resp.json())
+      .then((data) => {
+        let fromExchangeRate = data.conversion_rates[fromCurrency];
+        let toExchangeRate = data.conversion_rates[toCurrency];
+        const convertedAmount = (amount / fromExchangeRate) * toExchangeRate;
+        result.innerHTML = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(
+          2
+        )} ${toCurrency}`;
+      });
+  }
+  
+  if (amount.length != 0) {
+    fetchapi();
+  } else {
+    alert("you have to fill in the amount");
+  }
+};
+
+document
+  .querySelector("#convert-button")
+  .addEventListener("click", convertCurrency);
